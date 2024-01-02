d406d079a150c3c3b5c4d44c4f4351889c6304fa
paul <paul@stackadoc.com>
2024-01-02 19:00:23 +0100

    Rename db_models.py

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 from config import alembic_config
-from models import Base
+from db_models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
