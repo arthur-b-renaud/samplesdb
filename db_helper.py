f99ecc12ee7e4f1fd07ef7d2983d46b620130092
paul <paul@stackadoc.com>
2024-01-03 11:20:09 +0100

    Add album and other music metadata + re-create alembic migrations from scratch

--- db_helper.py
+++ db_helper.py
@@ -1,10 +1,10 @@
-from typing import List
+from typing import List, Tuple
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import Session, sessionmaker, joinedload
 
 from config.alembic_config import DB_URI
-from db_models import Artist, Sample, ArtistSample
+from db_models import Artist, Track, ArtistTrack, Album
 
 engine = create_engine(
     DB_URI,
@@ -23,9 +23,9 @@ def create_artist(session: Session, artist: Artist) -> Artist:
 
 
 def get_artist(
-    session: Session,
-    full_name: str = None,
-    artist_id: int = None,
+        session: Session,
+        full_name: str = None,
+        artist_id: int = None,
 ) -> Artist:
     query = session.query(Artist)
     if full_name is not None:
@@ -41,50 +41,142 @@ def get_artist(
     return artists[0]
 
 
+def create_album(session: Session, album: Album) -> Album:
+    session.add(album)
+    session.commit()
+    return album
+
+
+def get_album(
+        session: Session,
+        title: str = None,
+        album_id: int = None,
+) -> Album:
+    query = session.query(Album)
+    if title is not None:
+        query = query.where(Album.title == title)
+    if album_id is not None:
+        query = query.where(Album.id == album_id)
+
+    albums = query.all()
+    if len(albums) > 1:
+        raise ValueError("There is more than one album.")
+    if len(albums) == 0:
+        raise ValueError("There no album.")
+    return albums[0]
+
+
 def create_samples(
     session: Session,
-    samples: List[Sample],
-    artists: List[Artist],
-) -> List[Sample]:
-    # For each Sample and each Artist, create a relation many-to-many ArtistSample
-    for idx, sample in enumerate(samples):
-        samples[idx].artists_samples = []
-        for artist in artists:
-            samples[idx].artists_samples.append(ArtistSample(
-                artist=artist,
-                sample=sample,
-            ))
+    tracks_and_artists: List[Tuple[Track, List[Artist]]],
+) -> List[Track]:
+    """
+    Create multiple Samples
+    :param session: the connexion to the database
+    :param tracks_and_artists: List of tuples with Tracks and Artists of each track. Example:
+    >>> tracks_and_artists = [
+    >>>     (
+    >>>         Track(title="Dark serviettes", album=my_album),     # Track #1
+    >>>         [my_arist_1, my_arist_2],                           # Artists for the Track #1
+    >>>     ),
+    >>>     (
+    >>>         Track(title="Deep chenille", album=my_album),       # Track #2
+    >>>         [my_arist_1, my_arist_2],                           # Artists for the Track #2
+    >>>     ),
+    >>> ]
 
-    session.add_all(samples)
+    :return:
+    """
+    completed_tracks = []
+    # For each Track and each Artist, create a relation many-to-many ArtistTrack
+    for track, artists in tracks_and_artists:
+        completed_track = track
+        completed_track.artists_tracks = []
+        for artist in artists:
+            completed_track.artists_tracks.append(ArtistTrack(
+                artist=artist,
+                track=track,
+            ))
+        completed_tracks.append(completed_track)
+
+    session.add_all(completed_tracks)
     session.commit()
 
-    return samples
+    return completed_tracks
+
+
+def get_track(
+        session: Session,
+        title: str = None,
+        track_id: int = None,
+) -> Track:
+    query = session.query(Track)
+    if title is not None:
+        query = query.where(Track.title == title)
+    if track_id is not None:
+        query = query.where(Track.id == track_id)
+
+    tracks = (
+        query
+        .options(joinedload(Track.artists_tracks))
+        .all()
+    )
+    if len(tracks) > 1:
+        raise ValueError("There is more than one track.")
+    if len(tracks) == 0:
+        raise ValueError("There no track.")
+    return tracks[0]
 
 
 if __name__ == "__main__":
     # Create artist
-    with create_session() as conn:
-        created_artist = create_artist(artist=Artist(full_name="Patrick Sebastien"), session=conn)
+    # with create_session() as conn:
+    #     new_artist_1 = create_artist(artist=Artist(full_name="Patrick Sebastien"), session=conn)
+    #     new_artist_2 = create_artist(artist=Artist(full_name="Boris Breja"), session=conn)
 
     # Get artist
+    # with create_session() as conn:
+    #     created_artist_1 = get_artist(full_name="Patrick Sebastien", session=conn)
+    #     created_artist_2 = get_artist(artist_id=2, session=conn)
+
+    # Create album
+    # with create_session() as conn:
+    #     new_album = create_album(album=Album(title="My Album"), session=conn)
+
+    # Get album
+    # with create_session() as conn:
+    #     created_album = get_album(title="My Album", session=conn)
+    #     print(created_album)
+
+    # Create tracks
+    # with create_session() as conn:
+    #     my_arist_1 = get_artist(full_name="Boris Breja", session=conn)
+    #     my_arist_2 = get_artist(full_name="Patrick Sebastien", session=conn)
+    #
+    #     my_album = get_album(title="My Album", session=conn)
+    #
+    #     my_samples = [
+    #         (
+    #             Track(title="Dark serviettes", album=my_album),
+    #             [my_arist_1, my_arist_2],
+    #         ),
+    #         (
+    #             Track(title="Deep chenille", album=my_album),
+    #             [my_arist_1, my_arist_2],
+    #         ),
+    #     ]
+    #
+    #     created_samples = create_samples(
+    #         session=conn,
+    #         tracks_and_artists=my_samples,
+    #     )
+    #
+    #     print(created_samples)
+
+    # Get track
     with create_session() as conn:
-        created_artist_1 = get_artist(full_name="Boris Breja", session=conn)
-        created_artist_2 = get_artist(artist_id=3, session=conn)
+        my_track = get_track(title="Dark serviettes", session=conn)
+        print("Track:", my_track)
+        print("Artists:", [artist_track.artist for artist_track in my_track.artists_tracks])
+        print("Album:", my_track.album)
 
-    # Create samples
-    with create_session() as conn:
-        my_arist_1 = get_artist(full_name="Boris Breja", session=conn)
-        my_arist_2 = get_artist(full_name="Patrick Sebastien", session=conn)
-
-        my_samples = [
-            Sample(title="Dark serviettes"),
-            Sample(title="Deep chenille"),
-        ]
-
-        created_samples = create_samples(
-            session=conn,
-            samples=my_samples,
-            artists=[my_arist_1, my_arist_2],
-        )
-
-        print(created_samples)
--- db_models.py
+++ db_models.py
@@ -66,70 +66,102 @@ class Artist(IdBaseMixin, TimestampBaseMixin):
 
     full_name: str = field(default=None, metadata={"sa": Column(String)})
 
-    # Relation with ArtistSample
-    artists_samples: List[ArtistSample] = field(
+    # Relation with ArtistTrack
+    artists_tracks: List[ArtistTrack] = field(
         default_factory=list,
-        metadata={"sa": relationship("ArtistSample", lazy="noload")},
+        metadata={"sa": relationship("ArtistTrack", lazy="noload")},
     )
 
 
 @mapper_registry.mapped
 @dataclass
-class ArtistSample(IdBaseMixin, TimestampBaseMixin):
-    """Many-to-many relation between Artist and Sample"""
+class ArtistTrack(IdBaseMixin, TimestampBaseMixin):
+    """Many-to-many relation between Artist and Track"""
 
-    __tablename__ = "artist_sample"
+    __tablename__ = "artist_track"
     __sa_dataclass_metadata_key__ = "sa"
 
     # Relation with Artist
     artist_ref: int = field(
         default=None,
         metadata={
-            "sa": Column(ForeignKey("artist.id", ondelete="SET NULL"), index=True)
+            "sa": Column(ForeignKey("artist.id", ondelete="CASCADE"), index=True)
         },
     )
     artist: Artist = field(
         default=None,
-        metadata={"sa": relationship("Artist", back_populates="artists_samples")},
+        metadata={"sa": relationship("Artist", back_populates="artists_tracks")},
     )
 
-    # Relation with Sample
-    sample_ref: int = field(
+    # Relation with Track
+    track_ref: int = field(
         default=None,
-        metadata={
-            "sa": Column(ForeignKey("sample.id", ondelete="SET NULL"), index=True)
-        },
+        metadata={"sa": Column(ForeignKey("track.id", ondelete="CASCADE"), index=True)},
     )
-    sample: Sample = field(
+    track: Track = field(
         default=None,
-        metadata={"sa": relationship("Sample", back_populates="artists_samples")},
+        metadata={"sa": relationship("Track", back_populates="artists_tracks")},
     )
 
 
 @mapper_registry.mapped
 @dataclass
-class Sample(IdBaseMixin, TimestampBaseMixin):
+class Album(IdBaseMixin, TimestampBaseMixin):
 
-    __tablename__ = "sample"
+    __tablename__ = "album"
     __sa_dataclass_metadata_key__ = "sa"
 
-    # Relation with ArtistSample
-    artists_samples: List[ArtistSample] = field(
+    title: str = field(default=None, metadata={"sa": Column(String, nullable=False)})
+
+    # Relation with Track
+    tracks: List[Track] = field(
         default_factory=list,
-        metadata={"sa": relationship("ArtistSample", lazy="noload")},
+        metadata={"sa": relationship("Track", lazy="noload")},
+    )
+
+
+@mapper_registry.mapped
+@dataclass
+class Track(IdBaseMixin, TimestampBaseMixin):
+
+    __tablename__ = "track"
+    __sa_dataclass_metadata_key__ = "sa"
+
+    # Relation with ArtistTrack
+    artists_tracks: List[ArtistTrack] = field(
+        default_factory=list,
+        metadata={"sa": relationship("ArtistTrack", lazy="noload")},
+    )
+
+    # Relation with Album
+    album_ref: int = field(
+        default=None,
+        metadata={"sa": Column(ForeignKey("album.id", ondelete="CASCADE"), index=True)},
+    )
+    album: Album = field(
+        default=None,
+        metadata={"sa": relationship("Album", back_populates="tracks")},
     )
 
     title: str = field(default=None, metadata={"sa": Column(String, nullable=False)})
-    album_title: str = field(default=None, metadata={"sa": Column(String)})
 
     # Music metadata
     annotation: str = field(default=None, metadata={"sa": Column(String)})
     bpm: int = field(default=None, metadata={"sa": Column(Integer)})
-    rates_khz: float = field(default=None, metadata={"sa": Column(Float)})
-    bitrate_kbps: float = field(default=None, metadata={"sa": Column(Float)})
+    rates_hz: int = field(default=None, metadata={"sa": Column(Float)})
+    bitrate_bps: int = field(default=None, metadata={"sa": Column(Float)})
     nb_channels: int = field(default=None, metadata={"sa": Column(Integer)})
     bit_depth: int = field(default=None, metadata={"sa": Column(Integer)})
     duration_secs: int = field(default=None, metadata={"sa": Column(Integer)})
+    styles: List[str] = field(
+        default_factory=list, metadata={"sa": Column(ARRAY(String))}
+    )
+    instruments: List[str] = field(
+        default_factory=list, metadata={"sa": Column(ARRAY(String))}
+    )
+    moods: List[str] = field(
+        default_factory=list, metadata={"sa": Column(ARRAY(String))}
+    )
 
     # File storage
     wav_s3_url: str = field(default=None, metadata={"sa": Column(String)})
