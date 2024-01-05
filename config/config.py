660b2dd97bdc8cc86d104a914a2148389a6ceea2
arthur <arthur@stackadoc.com>
2024-01-05 18:48:33 +0100

    config.py : added a config file for interacting with the envs

---
+++ config/config.py
@@ -0,0 +1,11 @@
+import os
+
+# ========== AWS ================================================================================ #
+
+# Config
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_REGION_STACKADOC = "eu-west-3"
+
+# S3
+S3_BUCKET = ""
